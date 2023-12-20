e637c526548eb1535c88b7fe2a98fcf315966e85
bistaprapannabista <bistaprapannabista@gmail.com>
2023-12-20 22:59:12 +0545

    working on

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export const Card = ({ movie }) => {
   const { id, original_title, overview, poster_path } = movie;
   const image = poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : Backup;
   return (
-    <div>
+    <div onClick={() => sessionStorage.setItem("movie_title", movie?.title)}>
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3 hover:shadow-lg">
         <Link to={`/movie/${id}`}>
           <img className="rounded-t-lg" style={{ height: "350px" }} src={image} alt="" />
@@ -17,6 +17,6 @@ export const Card = ({ movie }) => {
 
         </div>
       </div>
-    </div>
+    </div >
   )
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
-export const useFetch =(apiPath, queryTerm="") => {
- const [data, setData] =useState([]);
- const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
-    useEffect(()=>{ 
-        async function fetchMovies(){ 
-          const response = await fetch(url);
-          const json = await response.json();
-          setData(json.results);
-        }
-        fetchMovies();
-      },[url])
-      return {data}
+import movies_ids from "./data";
+export const useFetch = (apiPath, queryTerm = "") => {
+  const [data, setData] = useState([]);
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(url);
+      const json = await response.json();
+      const filteredData = json?.results?.filter((item) => movies_ids.includes(item.id));
+      setData(filteredData);
+    }
+    fetchMovies();
+  }, [url])
+  return { data }
 }
